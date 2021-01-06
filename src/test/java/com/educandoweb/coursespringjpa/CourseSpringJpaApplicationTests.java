5d9c4940064d10f31a8ac1f9bf66f632af5fe643
Marcelo Felix <celofelix93@gmail.com>
2021-01-06 16:20:46 -0300

    Projeto criado

---
+++ src/test/java/com/educandoweb/coursespringjpa/CourseSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.coursespringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
