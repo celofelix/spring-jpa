9b645c02a6220fcc1afbbc22abec33fb5d41faa7
Marcelo Felix <celofelix93@gmail.com>
2021-01-07 13:23:14 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/spring/jpa/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.spring.jpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.spring.jpa.entities.Product;
+import com.educandoweb.course.spring.jpa.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		return product.get();
+	}
+	
+	
+}
