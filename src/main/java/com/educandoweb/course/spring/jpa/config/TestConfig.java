da9fe4a41a9b2eed2a08c281f84c1013977b7b45
Marcelo Felix <celofelix93@gmail.com>
2021-01-07 18:02:58 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/spring/jpa/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/spring/jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.course.spring.jpa.entities.Category;
 import com.educandoweb.course.spring.jpa.entities.Order;
 import com.educandoweb.course.spring.jpa.entities.OrderItem;
+import com.educandoweb.course.spring.jpa.entities.Payment;
 import com.educandoweb.course.spring.jpa.entities.Product;
 import com.educandoweb.course.spring.jpa.entities.User;
 import com.educandoweb.course.spring.jpa.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay);
+		
+		orderRepository.save(o1);
 		
 	}
 
